db1916acdd013af5a334a104468d8e4fd5c53db0
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-01-21 13:49:19 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ayanesh Sarkar on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
